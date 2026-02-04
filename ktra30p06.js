da2f1ee1e8a2bdfb982ec68ed60060a1f4c19492
khanh23K <khanhkinh23@gmail.com>
2026-02-04 07:40:43 +0700

    Add files via upload

---
+++ ktra30p06.js
@@ -0,0 +1,25 @@
+let hocSinh = ["Quý, Nam, Lan, Hùng, Nam"];
+
+let students = hocSinh.split(", ");
+
+console.log(students);
+
+students.reverse();
+console.log(students);
+
+let timKiem = "Lan";
+if (timKiem === "Lan") {
+    console.log("Tên Lan tồn tại trong mảng");
+} else {
+    console.log("Tên Lan không tồn tại trong mảng");
+}
+
+let mang = [100,200,300,400,500];
+for(let index of mang){
+    console.log(index);
+    
+}
+for(let text in mang){
+    console.log(text);
+    
+}
